018b0b004c80e84995f6e341ffd02999b2a0bd58
Josh Scharer <josh.scharer@icloud.com>
2026-04-09 03:35:53 -0500

    Add Shopify Buy Button embed to /templates page

--- api/index.js
+++ api/index.js
@@ -505,9 +505,80 @@ function renderTemplatesPage() {
             ═══════════════════════════════════════════════════════════════
             -->
             <div id="shopify-buy-btn" class="shopify-buy-btn-wrapper">
-              <!-- ↓ REPLACE THIS with your Shopify Buy Button embed code ↓ -->
-              <a href="#" class="cta-btn shopify-placeholder-btn">Buy Now &rarr;</a>
-              <!-- ↑ REPLACE THIS with your Shopify Buy Button embed code ↑ -->
+              <div id='product-component-1775723616563'></div>
+              <script type="text/javascript">
+              /*<![CDATA[*/
+              (function () {
+                var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+                if (window.ShopifyBuy) {
+                  if (window.ShopifyBuy.UI) {
+                    ShopifyBuyInit();
+                  } else {
+                    loadScript();
+                  }
+                } else {
+                  loadScript();
+                }
+                function loadScript() {
+                  var script = document.createElement('script');
+                  script.async = true;
+                  script.src = scriptURL;
+                  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+                  script.onload = ShopifyBuyInit;
+                }
+                function ShopifyBuyInit() {
+                  var client = ShopifyBuy.buildClient({
+                    domain: 'dz5m3m-e8.myshopify.com',
+                    storefrontAccessToken: '3c766236ce0998319a1473b5ce12127e',
+                  });
+                  ShopifyBuy.UI.onReady(client).then(function (ui) {
+                    ui.createComponent('product', {
+                      id: '11008480051473',
+                      node: document.getElementById('product-component-1775723616563'),
+                      moneyFormat: '%24%7B%7Bamount%7D%7D',
+                      options: {
+                        "product": {
+                          "styles": {
+                            "product": {
+                              "@media (min-width: 601px)": {
+                                "max-width": "100%",
+                                "margin-left": "0px",
+                                "margin-bottom": "0px"
+                              }
+                            }
+                          },
+                          "text": { "button": "Add to cart" }
+                        },
+                        "productSet": {
+                          "styles": {
+                            "products": {
+                              "@media (min-width: 601px)": { "margin-left": "0px" }
+                            }
+                          }
+                        },
+                        "modalProduct": {
+                          "contents": { "img": false, "imgWithCarousel": true, "button": false, "buttonWithQuantity": true },
+                          "styles": {
+                            "product": {
+                              "@media (min-width: 601px)": {
+                                "max-width": "100%",
+                                "margin-left": "0px",
+                                "margin-bottom": "0px"
+                              }
+                            }
+                          },
+                          "text": { "button": "Add to cart" }
+                        },
+                        "option": {},
+                        "cart": { "text": { "total": "Subtotal", "button": "Checkout" } },
+                        "toggle": {}
+                      },
+                    });
+                  });
+                }
+              })();
+              /*]]>*/
+              </script>
             </div>
 
           </div>
